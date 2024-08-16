00c37a47188e1b278b44e6075a2f48e0577de52d
khoapip <ngotheanhkhoa@gmail.com>
2024-08-16 21:34:36 +0700

    enter to submit

--- src/app/_components/messageForm.tsx
+++ src/app/_components/messageForm.tsx
@@ -9,6 +9,7 @@ import {GearIcon} from "@radix-ui/react-icons"
 import UserMessage from './usermessage'
 
 import classNames from 'classnames';
+import React, { KeyboardEventHandler } from 'react'
 
 
 export default function MessageForm({
@@ -24,30 +25,36 @@ export default function MessageForm({
    const { submitUserMessage } = useActions<typeof  AI>()
    const [_, setMessages] = useUIState<typeof AI>()
 
+   const handleSubmit = async  (e : React.FormEvent<HTMLFormElement>) =>{
+      e.preventDefault()
+      const value = input.trim()
+      setInput('')
+      if (!value) return
+
+      setMessages(currentMessages => [
+         ...currentMessages,
+         {
+           id: nanoid(),
+           role: 'user',
+           display: <UserMessage  content={value}/>
+         }
+       ])
+   
+       const responseMessage = await submitUserMessage(value)
+       setMessages(currentMessages => [...currentMessages, responseMessage])
+
+   }
+
+   const handleKeyDown = (event : React.KeyboardEvent<HTMLTextAreaElement>) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        handleSubmit(event as unknown as React.FormEvent<HTMLFormElement>); // Trigger form submission
+      }
+    };
+  
 
    return (
    <form
-      onSubmit={
-         async  (e : React.FormEvent<HTMLFormElement>) =>{
-            e.preventDefault()
-            const value = input.trim()
-            setInput('')
-            if (!value) return
-
-            setMessages(currentMessages => [
-               ...currentMessages,
-               {
-                 id: nanoid(),
-                 role: 'user',
-                 display: <UserMessage  content={value}/>
-               }
-             ])
-         
-             const responseMessage = await submitUserMessage(value)
-             setMessages(currentMessages => [...currentMessages, responseMessage])
- 
-         }
-      }
+      onSubmit={ handleSubmit}
    
    >
     <div className={classNames("fixed left-0 bottom-5 flex", widthType)}>
@@ -57,7 +64,13 @@ export default function MessageForm({
                    
                </div>
                <div className="w-full my-auto inline-flex max-w-[80%]">
-                  <Textarea value={input} onChange={e => setInput(e.target.value)}  placeholder="Message me" className="text-white min-h-[60px] bg-transparent w-full resize-none max-h-60 px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"/>
+                  <Textarea 
+                  value={input} 
+                  onChange={e => setInput(e.target.value)}  
+                  placeholder="Message me" 
+                  className="text-white min-h-[60px] bg-transparent w-full resize-none max-h-60 px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+                  onKeyDown={handleKeyDown}
+                  />
                </div>
                <div className="w-12 my-auto flex justify-start">
                   <IconButton  variant="outline" className='w-8 h-8 bg-slate-500 rounded-full  my-auto flex justify-start'>
