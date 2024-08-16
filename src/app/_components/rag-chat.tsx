e99588f8dc88342d979322341dd8c5d0b486d613
khoapip <ngotheanhkhoa@gmail.com>
2024-08-16 21:45:27 +0700

    add upload dropzone for rag

--- src/app/_components/rag-chat.tsx
+++ src/app/_components/rag-chat.tsx
@@ -4,6 +4,7 @@ import MessageForm from "./messageForm";
 import MessagePanel from "./messagePanel";
 import { useState } from "react";
 import { useAIState, useUIState } from "ai/rsc";
+import { UploadDropzone } from "@/app/_components/uploadthing";
 
 export default function RagChat(){
   const [input, setInput] = useState('');
@@ -12,7 +13,7 @@ export default function RagChat(){
   
   <div className="mt-20 w-full flex">
     <div className=" w-1/2  overflow-y-auto scrollbar-hide ">
-      <div className="flex flex-col max-w-2xl   mx-auto">
+      <div className="flex flex-col max-w-2xl mx-auto">
         <MessagePanel input={input}/>
           <MessageForm 
           widthType="w-1/2"
@@ -21,8 +22,20 @@ export default function RagChat(){
           />
       </div>
     </div>
-    <div className="w-1/2">
-
+    <div className="w-1/2 my-auto">
+      <UploadDropzone
+        className=""
+        endpoint="pdfUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />    
     </div>
   </div>
 
