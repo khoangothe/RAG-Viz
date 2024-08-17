a26684330e25f858778b89a29550b6777ba91677
khoapip <ngotheanhkhoa@gmail.com>
2024-08-18 03:01:16 +0700

    update pinecone integration

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,7 +2,20 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import {db} from  "@/server/db" 
 import { files } from "@/server/db/schema";
-import { Pinecone } from '@pinecone-database/pinecone'
+import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
+
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+
+import { Pinecone } from '@pinecone-database/pinecone';
+
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+
+import fs from "fs/promises";
 
 
 
@@ -18,26 +31,63 @@ export const pdfRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
-      const index_name = metadata.userId + "_" +  file.name;
-      try{
-        await db.insert(files).values(
-          {
+      // const index_name = metadata.userId.toLowerCase() + "-" +  file.name.toLocaleLowerCase();
+      const index_name = "test"
+      await db.insert(files).values({
             userid: metadata.userId,
             file_name: file.name,
             file_url: file.url,
             index_name: index_name
+        }
+      )
+      
+      try{
+        //Make this global
+        const pc = new Pinecone();
+        await pc.createIndex({
+          name: index_name,
+          dimension: 1536,
+          metric: 'cosine',
+          spec: {
+            serverless: {
+              cloud: 'aws',
+              region: 'us-east-1'
+            }
+          }
+        });
+
+        const response = await fetch(file.url);
+        const data = await response.blob();
+        const loader = new WebPDFLoader(data, {});
+        const docs = await loader.load();
+        //Should make this global like db
+        const splitter = new RecursiveCharacterTextSplitter({
+          chunkSize:1024,
+          chunkOverlap:0,
+          lengthFunction:(text) => {return text.length},
+        });
+        //Should make this global
+        const embeddings = new OpenAIEmbeddings({
+            batchSize: 512, // Default value if omitted is 512. Max is 2048
+            model: "text-embedding-ada-002"
           }
         )
-      } catch (error) {
-        console.error('Error adding file:', error);
+        const text = "PERSONAL PROJECTS";
+        
+        const finalDocs = await splitter.splitDocuments(docs)
+        const pinecone = new PineconeClient();
+        const pineconeIndex = pinecone.Index(index_name);
 
+
+        await PineconeStore.fromDocuments(
+          finalDocs,
+          embeddings,
+          {pineconeIndex : pineconeIndex}
+        );
+      }catch(error){
+        console.log(error);
       }
-
-
-
-
- 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
