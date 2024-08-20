7ba41baed639cccbf4d46e0bcbe54eae0d68f230
khoapip <ngotheanhkhoa@gmail.com>
2024-08-20 10:10:22 +0700

    update shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import { SidebarToggle } from "@/components/rag-panel/sidebar-toggle";
+import { useStore } from "@/hooks/use-store";
+import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
+
 export default function HomePage() {
-
+  const sidebar = useStore(useSidebarToggle, (state) => state);
   return (
-    <main className="flex h-screen w-full">
-
+    <main className="flex h-screen w-1/2">
+      <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
      </main>
   );
 }
--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,4 +1,11 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 import { customAlphabet } from 'nanoid'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 
 
 export const nanoid  = customAlphabet(
