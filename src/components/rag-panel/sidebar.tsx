4cd1b3c9b5d1c69b088ed57a4fc3f89c2032bf6b
khoapip <ngotheanhkhoa@gmail.com>
2024-08-22 23:34:05 +0700

    update UI

--- src/components/rag-panel/sidebar.tsx
+++ src/components/rag-panel/sidebar.tsx
@@ -20,7 +20,7 @@ export function Sidebar({files} : {files: FileType[]}) {
     <aside
       className={cn(
         "fixed top-0 right-0 z-20 h-screen translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300 bg-black",
-        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+        sidebar?.isOpen === false ? "w-[0px] hover:w-[90px]" : "w-72"
       )}
     >
       <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
