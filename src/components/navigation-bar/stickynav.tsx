d09e7c378ed426f21742084cbc5ab4dbe8d945a1
khoapip <ngotheanhkhoa@gmail.com>
2024-08-23 19:22:14 +0700

    fix mobile

--- src/components/navigation-bar/stickynav.tsx
+++ src/components/navigation-bar/stickynav.tsx
@@ -41,14 +41,14 @@ const links = [
     }
   ]
   
-  export const NavigationMenuDemo = () => {
+  export const NavigationMenus = () => {
     const pathname = usePathname();
     const currentLink = links.find(link => link.href === pathname) ?? links[0];
     const otherLinks = links.filter(link => link.href !== pathname);
 
     return (
-      <NavigationMenu.Root className="relative z-[1] flex w-full ">
-        <NavigationMenu.List className="m-0 flex list-none rounded-[6px] bg-transparent p-1">
+      <NavigationMenu.Root className="relative z-[1] flex w-full">
+        <NavigationMenu.List className="m-0 flex list-none rounded-[6px] bg-transparent p-1 ">
           <NavigationMenu.Item>
             <NavigationMenu.Trigger className="text-white hover:bg-red-400 focus:shadow-red-400 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
               {currentLink?.title}{' '}
@@ -57,8 +57,8 @@ const links = [
                 aria-hidden
               />
             </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-full sm:w-auto bg-red-500">
-              <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
+            <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute top-0 left-0 w-auto  bg-red-500">
+              <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] w-[400px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
                 <li className="row-span-3 grid">
                   <NavigationMenu.Link asChild>
                     <div
@@ -91,7 +91,7 @@ const links = [
         </NavigationMenu.List>
   
         <div className="perspective-[2000px] absolute top-full left-0 flex">
-          <NavigationMenu.Viewport className="data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
+        <NavigationMenu.Viewport className="relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] origin-[top_center] overflow-hidden rounded-[6px]  transition-[width,_height] duration-300 w-[400px] sm:w-[var(--radix-navigation-menu-viewport-width)]" />
         </div>
       </NavigationMenu.Root>
     );
@@ -128,7 +128,7 @@ export default function StickyNav(){
     return (
         <nav className="flex left-0 w-full  h-20 items-center pl-10 fixed top-0">
             <div className="flex w-full gap-3 justify-between">
-                <NavigationMenuDemo/>
+                <NavigationMenus/>
                 <SignedOut>
                   <div className='flex space-x-3 pr-5 -z-0'>
                   <Button className='rounded-full bg-white text-black' variant="outline">
